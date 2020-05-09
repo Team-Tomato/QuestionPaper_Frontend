da7d5dbdfba94520a06827179701d3ce032f3040
Akbar <akbar.h@samsung.com>
2020-05-09 17:40:22 +0530

    Project page arrangement

--- src/Views/projects.js
+++ src/Views/projects.js
@@ -1,127 +1,90 @@
 import React, { Component } from 'react';
-import img1 from '../images/tomatoo.jpg';
-import img2 from '../images/logo.jpg';
-import img3 from '../images/assist.png';
-import img4 from '../images/movie recommand.jpg';
-import img5 from '../images/thoughts of people.jpg';
+import extension from '../images/extension.png';
+import teamaction from '../images/gaction1.png';
+import movie from '../images/gaction2.png';
+import tableau from '../images/t20.png';
+import webapp from '../images/webapp.png';
+import flutter from '../images/flutter.png';
+import {
+  Row, Col, Card, CardImg, CardText, CardBody,
+  CardTitle, Button
+} from 'reactstrap';
+
+
 class Project extends Component {
   render() {
     return (
-      <div className="containers-fluid d-flex .justify content-md-center">
-                <div className="row">
-                <div className="col-md-4">
-                    <p className="pr-md-5">
-                    <p className="pl-md-5">
-                        <div className='card protext-center' style={{width: '200px'}}>
-                        <div className='card-Body text-dark'>
-                                <h5 className='project-Title'>Team Tomato</h5>
-                            <div  className='overflow'>
-                                <img className="card-img-top" src={img1} alt='Projectimage ' width="200" height="220"></img>
-                            </div>
-                           <p className='card-protext text-secondary'>
-                           Team which is determined to spread the love of open source (oss)
-                                </p>
-                                <a class="text-primary" href="https://github.com/Team-Tomato/" role="text">Team Tomato site</a>
-                                <p className='card-protext text-secondary'>
-                                Top languages used-Javascript,Python,HTML
-                                </p>
-                            </div>
-                        </div>
-                        </p>
-                        </p>
-                    </div>
-                   <div className="col-md-4">
-                    <p className="pr-md-5">
-                    <p className= "pl-md-5">
-                        <div className='card protext-center' style={{width: '200px'}}>
-                        <div className='card-Body text-dark'>
-                                <h5 className='project-Title'>One click for CEG</h5>
-                            <div  className='overflow'>
-                                <img className="card-img-top" src={img2} alt='Projectimage' width="200" height="220"></img>
-                            </div>
-                           <p className='card-protext text-secondary'>
-                           It is an extension which directs to all CEG related websites 
-                                </p>
-                                <a class="text-primary" href="https://github.com/Team-Tomato/oneClick-chrome-extension" role="text">Chrome Extension site</a>
-                                <p className='card-protext text-secondary'>
-                                Top languages used are-Javascript and HTML
-                                </p>
-                            </div>
-                        </div>
-                        </p>
-                        </p>
-                        </div>
-                    <div className="col-md-4">
-                    <p className="pr-md-5">
-                        <p className="pl-md-5">
-                        <div className='card protext-center' style={{width: '200px'}}>
-                        <div className='card-Body text-dark'>
-                                <h5 className='project-Title'>Team Tomato Action</h5>
-                            <div  className='overflow'>
-                                <img className="card-img-top" src={img3} alt='Projectimage' width="200" height="220"></img>
-                            </div>
-                           <p className='card-protext text-secondary'>
-                           Google Assistant action for Team Tomato
-                                </p>
-                                <a class="text-primary" href="https://github.com/Team-Tomato/GAction_TeamTomato" role="text">Assistant site</a>
-                                <p className='card-protext text-secondary'>
-                                This is built with the help of either C++ or Python
-                                </p>
-                            </div>
-                        </div>
-                        </p>
-                        </p>
-                        </div>
-                        <div className="col-md-6">
-                        <div className="float-md-right">
-                    <p className="pr-md-5">
-                    <p className="pl-md-5">
-                        <div className='card protext-center' style={{width: '200px'}}>
-                        <div className='card-Body text-dark'>
-                                <h5 className='project-Title'>Movie-recommandation Action</h5>
-                            <div  className='overflow'>
-                                <img className="card-img-top" src={img4} alt='Projectimage' width="200" height="220"></img>
-                            </div>
-                           <p className='card-protext text-secondary'>
-                                   ----- 
-                                </p>
-                                <a class="text-primary" href="https://github.com/Team-Tomato/Movie_Recommendation_Backend" role="text">Movie recommandation site</a>
-                                <p className='card-protext text-secondary'>
-                                Done using Python
-                                </p>
-                            </div>
-                        </div>
-                        </p>
-                        </p>
-                        </div>
-                    </div>
-                    <div className="col-md-6">
-                        <div className="float-md-left">
-                        <p className="pr-md-5">
-                            <p className="pl-md-5">
-                            <div className='card protext-center' style={{width: '200px'}}>
-                                <div className='card-Body text-dark'>
-                                    <h5 className='project-Title'>Thoughts of people</h5>
-                                    <div className='overflow'>
-                                        <img className="card-img-top" src={img5} alt='Projectimage' width="200" height="220"></img>
-                                    </div>
-                                    <p className='card-protext text-secondary'>
-                                        -------
-                                        </p>
-                                        <a class="text-primary" href="" role="text">People thoughts</a>
-                                        <p className='card-protext text-secondary'>
-                                            --------
-                                            </p>  
-                                    </div>
-                                </div>
-                        </p>
-                        </p>
-                    </div>
-                    </div>
-                    </div>
-                    </div>
-        );
-    }
+      <Row>
+        <Col md={2} lg={2} sm={6}>
+          <Card>
+            <CardImg src={webapp} alt="Card image cap" />
+            <CardBody>
+              <CardTitle>Web App</CardTitle>
+              <CardText>Team Tomato website</CardText>
+              <CardText>[ React.js, Flask, PostgreSQL ]</CardText>
+              <a href="https://github.com/Team-Tomato/QuestionPaper_Frontend" target="_blank"><Button style={{ backgroundColor: "#da4eda" }} variant="contained">Github Repo</Button></a>
+            </CardBody>
+          </Card>
+        </Col>
+        <Col md={2} lg={2} sm={6}>
+          <Card>
+            <CardImg src={teamaction} alt="Card image cap" />
+            <CardBody>
+              <CardTitle>G Action-1</CardTitle>
+              <CardText>Team Tomato Action</CardText>
+              <CardText>[ Node.js, Dialogflow, Canvas ]</CardText>
+              <a href="https://github.com/Team-Tomato/GAction_TeamTomato" target="_blank"><Button style={{ backgroundColor: "#da4eda" }} variant="contained">Github Repo</Button></a>
+            </CardBody>
+          </Card>
+        </Col>
+        <Col md={2} lg={2} sm={6}>
+          <Card>
+            <CardImg src={movie} alt="Card image cap" />
+            <CardBody>
+              <CardTitle>G Action-2</CardTitle>
+              <CardText>Movie Recommendation</CardText>
+              <CardText>[ Node.js, Dialogflow ]</CardText>
+              <a href="https://github.com/Team-Tomato/GAction_MovieRecommendation" target="_blank"><Button style={{ backgroundColor: "#da4eda" }} variant="contained">Github Repo</Button></a>
+            </CardBody>
+          </Card>
+        </Col>
+        <Col md={2} lg={2} sm={6}>
+          <Card>
+            <CardImg src={extension} alt="Card image cap" />
+            <CardBody>
+              <CardTitle>One Click Extension</CardTitle>
+              <CardText>Extension for CEG</CardText>
+              <CardText>[ HTML, CSS, Javascript ]</CardText>
+              <a href="https://github.com/Team-Tomato/oneClick-chrome-extension" target="_blank"><Button style={{ backgroundColor: "#da4eda" }} variant="contained">Github Repo</Button></a>
+            </CardBody>
+          </Card>
+        </Col>
+        <Col md={2} lg={2} sm={6}>
+          <Card>
+            <CardImg src={flutter} alt="Card image cap" />
+            <CardBody>
+              <CardTitle>Team Tomato App</CardTitle>
+              <CardText>Flutter Application</CardText>
+              <CardText>[ Flutter, Dart, PostgreSQL ]</CardText>
+              <Button style={{ backgroundColor: "#da4eda" }} variant="contained">In Progress</Button>
+            </CardBody>
+          </Card>
+        </Col>
+        <Col md={2} lg={2} sm={6}>
+          <Card>
+            <CardImg src={tableau} alt="Card image cap" />
+            <CardBody>
+              <CardTitle>Chat Bot</CardTitle>
+              <CardText>Chat bot for Maths Department</CardText>
+              <CardText>[ Node.js, Wit.ai ]</CardText>
+              <Button style={{ backgroundColor: "#da4eda" }} variant="contained">In Progress</Button>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+
+    );
+  }
 }
-      
+
 export default Project;
