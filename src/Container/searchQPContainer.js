3135110f3b02c28bd08bd3772de96081b4ae0903
Akbar <akbar.h@samsung.com>
2020-05-17 17:22:23 +0530

    Table view

--- src/Container/searchQPContainer.js
+++ src/Container/searchQPContainer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Card, CardBody, CardFooter } from 'reactstrap'
-import { Container, Row, Col } from 'reactstrap'
+import { Container, Row, Col, Table } from 'reactstrap'
 import FormQP from '../Component/searQPForm.js'
 import '../Styles/style.css'
 import Loader from 'react-loading';
@@ -39,72 +39,109 @@ class SearchQP extends Component {
         qpData: response,
         loading: false
       })
-      console.log(response)
+      console.log(response, "REs")
     })
   }
 
   render() {
     let QPContainer
     let urls = []
-    let urlList = []
-    let staffList = []
-    let shortFormList = []
-    this.state.qpData.forEach(element => {
-      staffList.push(element['shortForm'])
-      shortFormList.push(element['staff'])
-      const url='https://avatars2.githubusercontent.com/u/20479150?v=4/'
-      urlList.push(url)
-    });
-    console.log(shortFormList, staffList, urlList)
-    urls = urlList.map((images, index) => (
-      <Col lg={3} md={4} sm={12} key={index} className='cardPadding'>
-        <Card>
-          <CardBody>
-            <img className="img-align" src={images} alt="Card image cap" />
-            {/* <img src={images} alt='QP Image' /> */}
-          </CardBody>
-          <CardFooter>
-            Subject | StaffName
-                    {/* Need to make it dynamic */}
-          </CardFooter>
-        </Card>
-      </Col>
-    ))
-    if (this.state.loading === false) {
-      QPContainer =
-        <Container>
-          <Row>
-            {urls}
-          </Row> 
-        </Container>
+
+    //For Card Usgae
+    // let urlList = []
+    // let staffList = []
+    // let shortFormList = []
+
+    // this.state.qpData.forEach(element => {
+    //   staffList.push(element['shortForm'])
+    //   shortFormList.push(element['staff'])
+    //   const url='https://avatars2.githubusercontent.com/u/20479150?v=4/'
+    //   urlList.push(url)
+    // });
+    // console.log(shortFormList, staffList, urlList)
+    // urls = urlList.map((images, index) => (
+    //   <Col lg={3} md={4} sm={12} key={index} className='cardPadding'>
+    //     <Card>
+    //       <CardBody>
+    //         <img className="img-align" src={images} alt="Card image cap" />
+    //         {/* <img src={images} alt='QP Image' /> */}
+    //       </CardBody>
+    //       <CardFooter>
+    //         Subject | StaffName
+    //                 {/* Need to make it dynamic */}
+    //       </CardFooter>
+    //     </Card>
+    //   </Col>
+    // ))
+    let table = []
+
+    //Loading Option
+    if (this.state.qpData !== [] && (this.state.qpData).length !== 0) {
+
+      // if(qpData && qpData.length) {
+      table = this.state.qpData.map((data, index) => {
+        // console.log("D",data, data[1], data['staff'])
+        // url = data['url']
+        return (
+          <tr key={index}>
+            <td>{data['subjectName']}</td>
+            <td>{data['staff']}</td>
+            <td>{data['shortForm']}</td>
+            <a href={data['url']} target="blank"><td>{data['url']}</td></a>
+          </tr>
+        )
+      })
+      // }
+
+      if (this.state.loading === false) {
+        QPContainer =
+          <Container>
+            <Table striped hover responsive>
+              <thead>
+                <tr>
+                  <th>Subject Name</th>
+                  <th>Staff Name</th>
+                  <th>ShortForm</th>
+                  <th>QP Link</th>
+                </tr>
+              </thead>
+              <tbody>
+                {table}
+              </tbody>
+            </Table>
+          </Container>
+      }
+      else {
+        QPContainer = <div style={{
+          position: 'absolute', left: '50%', top: '92%',
+          transform: 'translate(-50%, -50%)'
+        }}>
+          <Loader type={"bars"} color={"black"} />
+        </div>
+      }
     }
     else {
-      QPContainer = <div style={{
-        position: 'absolute', left: '50%', top: '92%',
-        transform: 'translate(-50%, -50%)'
-      }}>
-        <Loader type={"bars"} color={"black"} />
-      </div>
+      QPContainer = <h6></h6>
     }
 
-    return (
-      <div>
-        <Card className="gradient">
-          <CardBody className ="welcome-title" style={{
-                position: 'absolute', left: '50%', top: '50%',
-                transform: 'translate(-50%, -50%)',
-                color:'white'
-              }}>
-            <h5>Search for integrated M.Sc question papers</h5>
-          </CardBody>
-        </Card>
-        
-        <FormQP handleSubmit={this.handleSubmit.bind(this)} />
-        <br />
-        {QPContainer}
-      </div>
-    )
-  }
-}
+      return (
+        <div>
+          <Card className="gradient">
+            <CardBody className="welcome-title" style={{
+              position: 'absolute', left: '50%', top: '50%',
+              transform: 'translate(-50%, -50%)',
+              color: 'white'
+            }}>
+              <h5>Search for integrated M.Sc question papers</h5>
+            </CardBody>
+          </Card>
 
-export default SearchQP;
+          <FormQP handleSubmit={this.handleSubmit.bind(this)} />
+          <br />
+          {QPContainer}
+        </div>
+      )
+    }
+  }
+
+  export default SearchQP;
