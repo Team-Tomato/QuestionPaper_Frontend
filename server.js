cd43d069a1dbaed369c29e296d859160fd4118e4
Akbar <akbar.h@samsung.com>
2020-05-21 00:45:15 +0530

    Express server to serve the built files

---
+++ server.js
@@ -0,0 +1,20 @@
+//Ref - https://medium.com/jeremy-gottfrieds-tech-blog/tutorial-how-to-deploy-a-production-react-app-to-heroku-c4831dfcfa08
+const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+const port = process.env.PORT || 3535;
+const app = express();
+
+app.use(favicon(__dirname + '/build/favicon.ico'));
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/wastepath', function (req, res) {
+ return res.send('Test working');
+});
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(port);
