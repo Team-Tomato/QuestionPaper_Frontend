6855832669c7f07e7883c8542d2042802ee13564
dinesh <dkdinesh6688@gmail.com>
2020-06-02 10:46:05 -0700

    template-initial

---
+++ src/Views/parta.js
@@ -0,0 +1,148 @@
+import React, { Component } from "react";
+import {Row,Col,Input, Label, Button} from 'reactstrap'
+import Partb from './partb'
+ 
+class Parta extends Component {
+  constructor(){
+    super();
+   
+    this.state={
+      subload:false,
+      sub:["a","b","c","d"],
+      qp:
+      [
+        {
+        question:"",
+        mark:"",
+      subqp:
+          [
+           
+          ]
+        }
+    ]
+    
+    }
+  }
+  //const [inputList, setInputList] = useState([{ question: "", mark: "" }]);
+ 
+  // handle input change
+   handleInputChange = (e, index) => {
+    const { name, value } = e.target;
+    const list = [...this.state.qp];
+    list[index][name] = value;
+    this.setState({qp:list})
+  };
+
+  handleInputsubChange = (e, index,subid) => {
+    const { name, value } = e.target;
+    this.state.qp[index].subqp[subid][name] = value;
+    this.setState(this.state.qp[index].subqp[subid])
+  };
+ 
+  // handle click event of the Remove button
+   handleRemoveClick = index => {
+    const list = [...this.state.qp];
+    list.splice(index, 1);
+    this.setState({qp:list})
+  };
+
+  handleRemovesubClick = (index,subid) => {
+    this.state.qp[index].subqp.splice(subid, 1);
+    this.setState(this.state.qp[index].subqp)
+  };
+ 
+  // handle click event of the Add button
+   handleAddClick = (e) => {
+    this.setState((prevState)=>({
+      qp:[...prevState.qp,{question:"",mark:"",subqp:[]}]
+    }))
+  };
+
+  handleSubClick = index => {
+   // console.log(index)
+    this.state.qp[index].subqp.push({question:"",mark:""})
+   // console.log(this.state.qp[index].subqp.length)
+    // this.setState((prevState)=>({
+    //   qp:[...prevState.qp,{subqp:[{question:"",mark:""}]}]
+    // }))
+    this.setState({subload:true})
+
+  };
+ 
+  render(){
+    //console.log(this.state.sub[0])
+    
+  return (
+    <div> 
+    <Row className="form-group row-align">
+                        <Col md={3} className="offset-md-4">
+                            <Label  className="form-control" id="part-a" align="center">PART-A</Label>
+                        </Col>
+                        
+                        <Col md={3} className="offset-md-1">
+                            <Input type="number" className="form-control" id="part-a_mark" placeholder="Total Mark"/>
+                        </Col>
+                </Row>
+      {
+      this.state.qp.map((x,id) => {
+        return (
+        <div><Row className="form-group" key={id}>
+        <Col md={1}>
+        
+            {this.state.qp.length !== 1 && 
+            <Button className="mr10" color="danger" onClick={() => this.handleRemoveClick(id)}>Del</Button>}
+        </Col>
+        <Col md={1}>
+            <Label type="number" className="form-control" id="q_no" name="id">{id+1}</Label>
+        </Col>
+        <Col md={7}>
+            <Input name="question" className="form-control" placeholder="Questions"
+                value={x.question} onChange={e => this.handleInputChange(e, id)}/>
+        </Col>
+        <Col md={1}>
+         
+            <Input type="number" className="form-control ml10" name="mark" placeholder="M"
+                value={x.mark} onChange={e => this.handleInputChange(e, id)}/>
+        </Col>
+        <Col md={1}>
+            {<Button className="form-control" onClick={()=>this.handleSubClick(id)}>Sub</Button>}
+        </Col>
+        <Col md={1}>
+            { <Button className="form-control" color="primary" onClick={this.handleAddClick}>Add</Button>}
+        </Col>
+       </Row>
+     {this.state.qp[id].subqp.map((x,subid)=><div><Row className="form-group" key={id}>
+        <Col md={1}>
+        
+            {
+            <Button className="mr10" color="danger" onClick={() => this.handleRemovesubClick(id,subid)}>Del</Button>}
+        </Col>
+        <Col md={1}>
+            <Label type="number" className="form-control" id="q_no" name="id">{id+1+"."+this.state.sub[subid]+")"}</Label>
+        </Col>
+        <Col md={7}>
+            <Input name="question" className="form-control" placeholder="Questions"
+                value={x.question} onChange={e => this.handleInputsubChange(e, id, subid)}/>
+        </Col>
+        <Col md={1}>
+         
+            <Input type="number" className="form-control ml10" name="mark" placeholder="M"
+                value={x.mark} onChange={e => this.handleInputsubChange(e, id)}/>
+        </Col>
+       </Row></div>)}
+       
+  </div>
+
+            
+        );
+        
+      }
+      )}
+      <hr/>
+      <Partb id={this.state.qp.length}/>     
+    </div>
+  );
+    }
+}
+ 
+export default Parta;
--- src/Views/templateQP.js
+++ src/Views/templateQP.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
-import {Card, CardBody} from 'reactstrap';
+import {Card, CardBody,Row,Input,Col,Form,Container} from 'reactstrap';
+import Parta from './parta'
+import '../Styles/qp-template.css'
 
 class App extends Component {
     render() {
         return (
+            <div>
             <Card className="gradient">
                 <CardBody className="welcome-title" style={{
                     position: 'absolute', left: '50%', top: '50%',
@@ -13,6 +16,47 @@ class App extends Component {
                     <h4>Work in Porgress. Check us at 01.06.2020</h4>
                 </CardBody>
             </Card>
+            <Container>
+          <Form onSubmit={this.handleSubmit} onChange={this.handleChange}>
+          <Row className="form-group row-align">
+              <Col md={3} className=" offset-md-9">
+                <Input type="date" className="form-control" onChange={this.handleChange} id="date" name="date" placeholder="Date"/>
+              </Col>
+          </Row>
+          <Row className="form-group row-align">
+              <Col md={6}>
+                <Input type="text" className="form-control" onChange={this.handleChange} id="subject" name="subject" placeholder="Subject"/>
+              </Col>
+          </Row>
+          <Row className="form-group row-align">
+              <Col md={3}>
+                <Input type="text" className="form-control" onChange={this.handleChange} id="semester" name="semester" placeholder="Semester"/>
+              </Col>
+          </Row>
+          <Row className="form-group row-align">
+              <Col md={6}>
+                <Input type="text" className="form-control" onChange={this.handleChange} id="course" name="course" placeholder="Course"/>
+              </Col>
+          </Row>
+          <Row className="form-group row-align">
+              <Col md={3}>
+                <Input type="text" className="form-control" onChange={this.handleChange} id="regulation" name="regulation" placeholder="Regulation"/>
+              </Col>
+          </Row>
+          <Row className="form-group">
+              <Col md={3}>
+                <Input type="text" className="form-control" onChange={this.handleChange} id="time" name="time" placeholder="Time"/>
+              </Col>
+              <Col md={3} className="offset-md-6">
+              <Input type="number" className="form-control" onChange={this.handleChange} id="mark" name="mark" placeholder="Total Marks"/>
+              </Col>
+          </Row>
+          <hr/>
+          <Parta/>
+          <hr/>
+        </Form>
+      </Container>
+            </div>
         );
     }
 }
