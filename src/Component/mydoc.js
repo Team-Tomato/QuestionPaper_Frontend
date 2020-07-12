bd175ab9f8eb172fdbc346fcf248dcb301f7775d
= <=>
2020-07-12 08:33:32 +0530

    demo1

--- src/Component/mydoc.js
+++ src/Component/mydoc.js
@@ -5,30 +5,59 @@ import Popup from 'reactjs-popup';
 import Scroll from 'react-scroll';
 import { PDFExport } from "@progress/kendo-react-pdf";
 import subdivide from '../images/subdivide.png'
+import MathJax from 'react-mathjax2'
+import html2canvas from 'html2canvas'
+
+ 
+const styles = StyleSheet.create({
+  page: {
+    flexDirection: 'row',
+    backgroundColor: '#E4E4E4',
+  },
+  section: {
+    margin: 10,
+    padding: 10,
+    flexGrow: 1,
+  }
+})
+
  class mydoc extends Component {
      constructor(props)
      {
          super(props);
      }
-     exportPDFWithComponent = event => {
-        this.pdfExportComponent.save();
+    
+    exportPDFWithComponent() {
+      const element = document.querySelector("#pdf")
+
+        // this.pdfExportComponent.save();
     };
+
+    async componentDidMount() {
+      const script = document.createElement("script");
+      script.src = "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=default";
+      script.type = "text/x-mathjax-config"
+      script.value = MathJax.Hub.Config({
+        extensions: ["tex2jax.js", "TeX/AMSmath.js"],
+        jax: ["input/TeX", "output/SVG"],
+      });
+      document.body.appendChild(script);
+     
+      // MathJax.Hub.Register.StartupHook("MathMenu Ready",function () {
+      //   MathJax.Hub.Config({
+      //     extensions: ["tex2jax.js", "TeX/AMSmath.js"],
+      //     jax: ["input/TeX", "output/SVG"],
+      //   });
+      //   MathJax.Menu.BGSTYLE["z-index"] = 2001;
+      // });
+      
+    }
+   
     render() {
         return (
-            <div>
-              <Popup
-          open={this.props.open}
-          modal
-          closeOnDocumentClick
-          onClose={this.props.closeModal}
-          style={{overflow:'scroll'}}
-        >
-          
-         <Scroll.Element className="element" style={{overflow:'scroll',position:'relative',height:'620px'}}> 
+        
+          <div id="pdf">
           <div>
-            <a className="close" onClick={this.props.closeModal}>
-              &times;
-            </a>
             <PDFExport
                         ref={component => (this.pdfExportComponent = component)}
                         paperSize="auto"
@@ -47,6 +76,10 @@ import subdivide from '../images/subdivide.png'
               </Col>
           </Row>
           <Row className="form-group row-align">
+          <div>$$a sum_(i=1)^n i^3=((n(n+1))/2)^2 $$</div>
+          <div>$$a asqrtb $$</div>
+          <div>$$a sum_(i=1)^n i^3=((n(n+1))/2)^2 $$</div>
+
           <Label style={{textAlign:'center'}}>{"Subject : "+this.props.header.subject}</Label>
           </Row>
           <Row className="form-group row-align"> 
@@ -218,11 +251,11 @@ import subdivide from '../images/subdivide.png'
             </PDFExport>
             </div>
             
-            </Scroll.Element>
             
             <Button onClick={this.exportPDFWithComponent} style={{alignItems:'center'}}>Generate PDF</Button>
-            </Popup>
             </div>
+            
+            
         )
     }
 }
